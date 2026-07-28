3f389d8786a6b3bca7cc0fd35136801cb25882c7
Orest Kreminskyi <orest@headeroverride.com>
2026-07-28 12:29:12 +0300

    Added product hunt badge
    Signed-off-by: Orest Kreminskyi <orest@headeroverride.com>

--- app/SiteFooter.tsx
+++ app/SiteFooter.tsx
@@ -8,6 +8,19 @@ export default function SiteFooter() {
           <a href="https://github.com/headeroverride/headeroverride" target="_blank" rel="noreferrer">GitHub</a>
           <a href="https://www.youtube.com/@HeaderOverrideExtension" target="_blank" rel="noreferrer">YouTube</a>
           <a href="/contact">Contact</a>
+          <a
+            className="product-hunt-badge"
+            href="https://www.producthunt.com/products/header-override?embed=true&utm_source=badge-featured&utm_medium=badge&utm_campaign=badge-header-override"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              alt="Header Override - Browser extension to modify headers and cookies | Product Hunt"
+              width="250"
+              height="54"
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=1206875&theme=light&t=1785230338704"
+            />
+          </a>
         </nav>
       </div>
     </footer>
